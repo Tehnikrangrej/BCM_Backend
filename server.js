081cbe825a0f842887c78c95058217ab9be246d2
Tehnikrangrej <irshadrangreh@gmail.com>
2026-01-13 11:36:32 +0530

    Health endpoint added

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = require("./src/app");
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
