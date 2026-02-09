3a76259d766d2a4a2de0220281510128de99f25a
Tehnikrangrej <irshadrangreh@gmail.com>
2026-02-09 15:59:35 +0530

    n

--- utils/cloudinaryUpload.js
+++ utils/cloudinaryUpload.js
@@ -4,10 +4,15 @@ const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
-    folder: "bank_change_requests",
-    resource_type: "auto", // pdf, image, doc, etc
-    allowed_formats: ["jpg", "jpeg", "png", "pdf"],
+  params: async (req, file) => {
+    const isPdf = file.mimetype === "application/pdf";
+
+    return {
+      folder: "bank_change_requests",
+      resource_type: isPdf ? "raw" : "auto", // ⭐ IMPORTANT
+      use_filename: true,
+      unique_filename: true,
+    };
   },
 });
 
